c010432f252b90b13f911cc8688fa765e3e23082
phat.do <dotanphat20@gmail.com>
2023-06-11 11:25:48 +0700

    timing group hover

--- src/components/Award.jsx
+++ src/components/Award.jsx
@@ -19,7 +19,7 @@ const Award = () => {
               data-aos="fade-up"
               data-aos-delay={i * 450}
               className="min-w-[14rem] duration-300 cursor-pointer border-2 border-slate-200 rounded-xl text-center bg-bg_light_primary p-6 flex-1
-              outline-none outline-offset-0 hover:transition-all hover:outline outline-2 hover:outline-[#2CAAA0] shadow-lg shadow-cyan-500/50"
+              outline-none outline-offset-0 hover:!transition-all hover:!outline hover:!outline-2 hover:!outline-[#2CAAA0] group-hover:outline-none shadow-cyan-500/50"
             >
               <img src={content.logo} alt="..." className="mx-auto" />
               <p className="leading-7">{content.para}</p>
--- src/components/Cetification.jsx
+++ src/components/Cetification.jsx
@@ -18,8 +18,8 @@ const Cetification = () => {
               key={i}
               data-aos="fade-up"
               data-aos-delay={i * 450}
-              className="min-w-[14rem] hover:duration-300 cursor-pointer border-2 border-slate-200 rounded-xl text-center bg-bg_light_primary p-6 flex-1
-              outline-none outline-offset-0 hover:transition-all hover:outline outline-2 hover:outline-[#2CAAA0] shadow-lg shadow-cyan-500/50"
+              className="min-w-[14rem] duration-300 cursor-pointer border-2 border-slate-200 rounded-xl text-center bg-bg_light_primary p-6 flex-1
+              outline-none outline-offset-0 hover:!transition-all hover:!outline hover:!outline-2 hover:!outline-[#2CAAA0] group-hover:outline-none shadow-cyan-500/50"
             >
               <img src={content.logo} alt="..." className="mx-auto" />
               <h6 className="my-3">{content.title}</h6>
