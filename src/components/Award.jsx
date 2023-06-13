516df2c8739c74d5afc2ae2c978b6ae3a182f37b
phat.do <dotanphat20@gmail.com>
2023-06-13 14:30:33 +0700

    Changes commit in commit
    dischangews _ 1
    
    dischabges _2

--- src/components/Award.jsx
+++ src/components/Award.jsx
@@ -12,20 +12,6 @@ const Award = () => {
           {Award.subtitle}
         </h4>
         <br />
-        <div className="flex gap-5 justify-between flex-wrap group">
-          {Award.award_content.map((content, i) => (
-            <div
-              key={i}
-              data-aos="fade-up"
-              data-aos-delay={i * 450}
-              className="min-w-[14rem] duration-300 cursor-pointer border-2 border-slate-200 rounded-xl text-center bg-bg_light_primary p-6 flex-1
-              outline-none outline-offset-0 hover:!transition-all hover:!outline hover:!outline-2 hover:!outline-[#2CAAA0] group-hover:outline-none shadow-cyan-500/50"
-            >
-              <img src={content.logo} alt="..." className="mx-auto" />
-              <p className="leading-7">{content.para}</p>
-            </div>
-          ))}
-        </div>
       </div>
     </section>
   );
